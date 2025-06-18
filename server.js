b4b07b65080da318da17a67d38fd1fc25f33091b
xwby <160570513+xwbyy@users.noreply.github.com>
2025-06-18 12:32:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -30,10 +30,7 @@ app.use((req, res, next) => {
 
 // Handle all other routes by serving the index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/fitur', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'fitur', 'index.html'));
 });
 
 app.listen(port, () => {
