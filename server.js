61d2b655202853c6a70be784e61d4eb60d89f8ce
xwby <160570513+xwbyy@users.noreply.github.com>
2025-06-18 12:23:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'fitur', 'index.html'));
+  res.sendFile(path.join(__dirname, '/fitur', 'index.html'));
 });
 
 app.listen(port, () => {
