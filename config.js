b7f3bddedd11866dfd6b94c8c4b9b58bf3ecf25c
xwby <160570513+xwbyy@users.noreply.github.com>
2025-11-18 00:23:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,93 @@
+// config.js 
+const toolsConfig = [
+    {
+        name: "Instagram Downloader",
+        icon: "fab fa-instagram",
+        path: "/downloader/instagramdl.html"
+    },
+    {
+        name: "Twitter Downloader",
+        icon: "fab fa-twitter",
+        path: "/downloader/twitdl.html"
+    },
+    {
+        name: "TikTok Downloader",
+        icon: "fab fa-tiktok",
+        path: "/downloader/tiktokdl.html"
+    },
+    {
+        name: "YouTube Downloader",
+        icon: "fab fa-youtube",
+        path: "/downloader/ytb.html"
+    },
+    {
+        name: "Sticker Brat",
+        icon: "fas fa-sticky-note",
+        path: "/sticker/brat.html"
+    },
+    {
+        name: "Sticker Quote",
+        icon: "fas fa-quote-right",
+        path: "/sticker/qc.html"
+    },
+    {
+        name: "Remini Foto",
+        icon: "fas fa-expand-arrows-alt",
+        path: "/tools/remini.html"
+    },
+    {
+        name: "Image To Anime",
+        icon: "fas fa-magic",
+        path: "/tools/toanime.html"
+    },
+    {
+        name: "Facebook Downloader",
+        icon: "fas fa-object-ungroup",
+        path: "/downloader/facebookdl.html"
+    },
+    {
+        name: "Spotify Tool",
+        icon: "fab fa-spotify",
+        path: "/downloader/spotify.html"
+    },
+    {
+        name: "Hitamkan foto",
+        icon: "fas fa-user-secret",
+        path: "/tools/hitamkan.html"
+    },
+    {
+        name: "Pinterest Downloader",
+        icon: "fas fa-eraser",
+        path: "/downloader/pinterest.html"
+    },
+    {
+        name: "MediaFire Tools",
+        icon: "fas fa-file-alt",
+        path: "/downloader/MediaFire.html"
+    },
+    {
+        name: "Text To Qr",
+        icon: "fas fa-exchange-alt",
+        path: "/tools/txt2qr.html"
+    },
+    {
+        name: "Threads Downloader",
+        icon: "fas fa-image",
+        path: "/downloader/threads.html"
+    },
+    {
+        name: "Kalender",
+        icon: "fas fa-calendar-alt",
+        path: "/tools/kalender.html"
+    },
+    {
+        name: "Lacak Paket",
+        icon: "fas fa-truck",
+        path: "/checker/lacakpaket.html"
+    },
+    {
+        name: "Cek NIM",
+        icon: "fas fa-id-card",
+        path: "/checker/ceknim.html"
+    }
+];
